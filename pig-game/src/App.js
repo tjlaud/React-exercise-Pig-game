2af58d8e811c7e942d84a656ef7cf0680515c426
tjlaud <62766674+tjlaud@users.noreply.github.com>
2021-04-14 10:37:47 +0100

    init

---
+++ pig-game/src/App.js
@@ -0,0 +1,28 @@
+import "./App.css";
+import PlayerPanel from "./components/PlayerPanel";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="wrapper">
+        <button className="btn-new">New game</button>
+        <button className="btn-roll">Roll dice</button>
+        <button class="btn-hold">Hold</button>
+        <h1>Pig Game</h1>
+        <PlayerPanel playerNumber="1" />
+        <PlayerPanel playerNumber="2" />
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
+// Bring the Pig Game into this and turn it into a React app
+
+// Background = app
+// Heading    = app
+// Player panel = component
+// Dice       = component
+// New game button = app?
+// Roll button =  app?
---
+++ pig-game/src/components/PlayerPanel.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "../App.css";
+
+function PlayerPanel(props) {
+  return (
+    <div>
+      <h2>Player {props.playerNumber}</h2>
+      <h2>{props.currentScore}</h2>
+      <div className="scoreBox">
+        <h2>{props.globalScore}</h2>
+      </div>
+    </div>
+  );
+}
+
+export default PlayerPanel;
