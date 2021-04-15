3a725f75948dacb52206b7b0358df5f8491d7d8b
tjlaud <62766674+tjlaud@users.noreply.github.com>
2021-04-15 10:51:18 +0100

    activeplayer and current score

--- pig-game/src/App.js
+++ pig-game/src/App.js
@@ -4,27 +4,61 @@ import PlayerPanel from "./components/PlayerPanel";
 import Dice from "./components/Dice";
 
 function App() {
-  const [dieNumber1, setDieNumber1] = useState(1);
-  const [dieNumber2, setDieNumber2] = useState(1);
+  const [dieNumber1, setDieNumber1] = useState(2);
+  const [dieNumber2, setDieNumber2] = useState(2);
+  const [currentScore, setCurrentScore] = useState(0);
+  const [activePlayerOne, setActivePlayerOne] = useState(true);
 
-  // Die rolling function
+  // For next lesson:
+  // Active player is working.
+  // Current score still display the last roll NOT the current :(
+  // The current score needs reseting to 0 on a change of player.
+  // Hold button implementation.
+
   // Scores
   // Round score
   // Active player
   // Game playing?
+  // if game playing ? then allow the roll function and scores to work
+  // const newGame = () => {
+  //   setCurrentScore(0);
+  //   gamePlaying = !gamePlaying;
+  // };
 
+  // Die rolling function
   const rollFunction = () => {
     setDieNumber1(Math.floor(Math.random() * 6) + 1);
     setDieNumber2(Math.floor(Math.random() * 6) + 1);
+    console.log(dieNumber2);
+
+    if (dieNumber1 !== 1 && dieNumber2 !== 1) {
+      setCurrentScore(currentScore + dieNumber1 + dieNumber2);
+    } else {
+      nextPlayer();
+      // next player function
+      // console.log("next player");
+    }
   };
+
+  const nextPlayer = () => {
+    setActivePlayerOne(!activePlayerOne);
+    console.log(activePlayerOne);
+  };
+  // Current Score
 
   return (
     <div className="App">
       <div className="wrapper">
         <button className="btn-new">New game</button>
         <h1>Pig Game</h1>
-        <PlayerPanel playerNumber="1" />
-        <PlayerPanel playerNumber="2" />
+        <PlayerPanel
+          playerNumber="1"
+          currentScore={activePlayerOne ? currentScore : null}
+        />
+        <PlayerPanel
+          playerNumber="2"
+          currentScore={activePlayerOne ? null : currentScore}
+        />
         <Dice dieNumber={dieNumber1} />
         <Dice dieNumber={dieNumber2} />
         <br />
--- pig-game/src/components/PlayerPanel.jsx
+++ pig-game/src/components/PlayerPanel.jsx
@@ -5,7 +5,7 @@ function PlayerPanel(props) {
   return (
     <div>
       <h2>Player {props.playerNumber}</h2>
-      <h2>{props.currentScore}</h2>
+      <h2>Current: {props.currentScore}</h2>
       <div className="scoreBox">
         <h2>{props.globalScore}</h2>
       </div>
