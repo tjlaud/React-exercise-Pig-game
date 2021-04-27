990502b085666681a67717bcdf58112aaee7e9a8
tjlaud <62766674+tjlaud@users.noreply.github.com>
2021-04-27 17:55:10 +0100

    class names vs active player

--- pig-game/src/components/PlayerPanel.jsx
+++ pig-game/src/components/PlayerPanel.jsx
@@ -2,11 +2,15 @@ import React from "react";
 import "../App.css";
 
 function PlayerPanel(props) {
+  const handleClassName = (bool) => {
+    return bool === true ? "playerOneBox" : null;
+  };
+
   return (
     <div>
       <h2>Player {props.playerNumber}</h2>
       <h2>Current: {props.currentScore}</h2>
-      <div className="scoreBox">
+      <div className={handleClassName(props.activePlayerStyles)}>
         <h2>{props.totalScore1}</h2>
         <h2>{props.totalScore2}</h2>
       </div>
